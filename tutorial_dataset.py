bb28ff8a7e1da07db022188f57b868e22c0e8e2b
Alexander Nikolaev <137407036+Alexnik1990@users.noreply.github.com>
2024-09-29 10:44:12 +0300

    Update tutorial_dataset.py

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -6,9 +6,14 @@ from torch.utils.data import Dataset
 
 
 class MyDataset(Dataset):
-    def __init__(self):
+    def __init__(self, data_path):
+        """
+        Параметры:
+        - data_path: путь к папке, в которой находятся данные
+        """
         self.data = []
-        with open('./training/fill50k/prompt.json', 'rt') as f:
+        self.data_path = data_path  # Сохраняем переданный путь
+        with open(f'{self.data_path}/prompt.json', 'rt') as f:
             for line in f:
                 self.data.append(json.loads(line))
 
@@ -22,18 +27,21 @@ class MyDataset(Dataset):
         target_filename = item['target']
         prompt = item['prompt']
 
-        source = cv2.imread('./training/fill50k/' + source_filename)
-        target = cv2.imread('./training/fill50k/' + target_filename)
+        # Формируем полные пути к файлам
+        source = cv2.imread(f'{self.data_path}/{source_filename}')
+        target = cv2.imread(f'{self.data_path}/{target_filename}')
 
-        # Do not forget that OpenCV read images in BGR order.
+        # Не забываем, что OpenCV читает изображения в формате BGR.
         source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
 
-        # Normalize source images to [0, 1].
+        # Нормализуем изображение source в диапазон [0, 1].
         source = source.astype(np.float32) / 255.0
 
-        # Normalize target images to [-1, 1].
+        # Нормализуем изображение target в диапазон [-1, 1].
         target = (target.astype(np.float32) / 127.5) - 1.0
 
         return dict(jpg=target, txt=prompt, hint=source)
 
+# Пример использования
+# dataset = MyDataset(data_path='./training/fill50k')
